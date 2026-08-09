a43431d9b491f57da4a34bcfe0c89720e229b00b
bike28 <amokhantech@proton.me>
2026-08-09 11:52:29 +0300

    Update amkhn.sh

--- amkhn.sh
+++ amkhn.sh
@@ -15,7 +15,7 @@ fi
 # ============================================================================
 SSHD_PORT=22
 SLOWDNS_PORT=5300
-GITHUB_BASE="https://raw.githubusercontent.com/bike28/dns"
+GITHUB_BASE="https://raw.githubusercontent.com/bike28/main/dns"
 
 # ============================================================================
 # MODERN COLORS & DESIGN
